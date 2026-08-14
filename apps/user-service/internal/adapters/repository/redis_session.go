462e16cd991713664a5f36a5f55a9bd0dcb06f8b
Nes224 <Nes224@hotmail.com>
2026-08-14 11:12:00 +0700

    feat: redis session

--- apps/user-service/internal/adapters/repository/redis_session.go
+++ apps/user-service/internal/adapters/repository/redis_session.go
@@ -1,1 +1,70 @@
 package repository
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"hermes-ecommerce-platform/apps/user-service/internal/core/ports"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+type redisRepository struct {
+	client *redis.Client
+}
+
+func NewRedisRepository(client *redis.Client) redisRepository {
+	return redisRepository{
+		client: client,
+	}
+}
+
+func formatSessionKey(sessionID uuid.UUID) string {
+	return fmt.Sprintf("session:%s", sessionID.String())
+}
+
+func (r *redisRepository) CreateSession(ctx context.Context, session *ports.Session, duration time.Duration) error {
+	key := formatSessionKey(session.ID)
+	
+	data, err := json.Marshal(session)
+	if err != nil {
+		return fmt.Errorf("failed to marshal session: %w", err)
+	}
+
+	err = r.client.Set(ctx, key, data, duration).Err()
+	if err != nil {
+		return fmt.Errorf("failed to save session to redis: %w", err)
+	}
+	return nil
+}
+
+func (r *redisRepository) GetSession(ctx context.Context, id uuid.UUID) (*ports.Session, error) {
+	key := formatSessionKey(id)
+
+	val, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		if err == redis.Nil {
+			return nil, fmt.Errorf("session not found or expired")
+		}
+		return nil, fmt.Errorf("failed to get session from redis: %w", err)
+	}
+
+	var session ports.Session
+	err = json.Unmarshal([]byte(val), &session)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal session: %w", err)
+	}
+
+	return &session, nil
+}
+
+func (r *redisRepository) DeleteSession(ctx context.Context, id uuid.UUID) error {
+	key := formatSessionKey(id)
+	err := r.client.Del(ctx, key).Err()
+	if err != nil {
+		return fmt.Errorf("failed to delete session from redis: %w", err)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.10.0
+	github.com/redis/go-redis/v9 v9.22.0
+	golang.org/x/crypto v0.48.0
 	golang.org/x/time v0.15.0
 )
 
@@ -15,6 +17,7 @@ require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
@@ -37,8 +40,8 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/arch v0.23.0 // indirect
-	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.35.0 // indirect
